49c360c92500e9c618495352d357486eaa161722
Will Farley <wfarley@amazon.com>
2017-03-28 08:24:07 -0700

    Add webpack config

---
+++ webpack-config/development.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const PROJECT_ROOT = path.resolve(__dirname, '..');
+
+export default {
+  devtool: 'inline-source-map',
+  entry: path.resolve(PROJECT_ROOT, 'src/client/index'),
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      include: [PROJECT_ROOT],
+      loaders: ['babel-loader'],
+      test: /\.js$/,
+    }],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(PROJECT_ROOT, 'dist'),
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(PROJECT_ROOT, 'src/client/assets/index.html'),
+    }),
+  ],
+};
