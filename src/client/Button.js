0b908deb5de4dceeaa7655d4529387d477476046
Will Farley <wfarley@amazon.com>
2017-04-01 17:14:13 -0700

    Set step-3 starting point

--- src/client/Button.js
+++ src/client/Button.js
@@ -2,21 +2,26 @@
  * Button.js
  */
 
-const setup = text => {
+const setup = (text, data = {}) => {
   const elem = document.createElement('button');
   elem.innerText = text;
   elem.className = 'Button';
+  for (const field in data) {
+    elem.dataset[field] = data[field];
+  }
   return elem;
 };
 
 export default class Button {
 
-  constructor({text}) {
-    this.elem = setup(text);
+  constructor({text, data}) {
+    this.elem = setup(text, data);
   }
 
   addEventListener = (event, listener) => {
     this.elem.addEventListener(event, listener);
     return () => this.elem.removeEventListener(event, listener);
   }
+
+  destroy = () => this.parentElement.removeChild(this.elem);
 }
