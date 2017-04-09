0be5fc5ec201a9f2af5b1a5a27e153e35b8b72bd
Will Farley <wfarley@amazon.com>
2017-04-09 14:58:50 -0700

    Add production server

--- src/server/development.js
+++ src/server/development.js
@@ -4,6 +4,7 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('../../webpack-config/development');
 const socketIO = require('socket.io');
+const onSocketConnect = require('./socket');
 
 // port
 const port = 3000;
@@ -38,52 +39,4 @@ const server = app.listen(port, error => {
 
 const io = socketIO(server);
 
-// fake "users" database
-const users = {};
-
-/**
- * adds {username: id} to the users object
- * @param {string} username - username
- * @param {string} id - socket.id
- * @returns {function} removeUser - removeUser() function removes user from "users"
- */
-const addUser = (username, id) => {
-  users[username] = id;
-  return () => {
-    delete users[username];
-  };
-};
-
-io.on('connection', socket => {
-  // listen for "LOGIN" events and update user object
-  let logout;
-  socket.on('LOGIN', ({username}) => {
-    // make sure username isn't already taken
-    const usernameAvailable = !Object.prototype.hasOwnProperty.call(users, username);
-    if (usernameAvailable) {
-      // map username to id
-      logout = addUser(username, socket.id);
-      // emit "UPDATE_USER_LIST" to all clients
-      io.emit('UPDATE_USER_LIST', {users});
-    } else {
-      socket.emit('USERNAME_TAKEN');
-    }
-  });
-
-  // listen for "disconnect" event and remove user from "users" object
-  socket.on('disconnect', () => {
-    if (typeof logout === 'function') logout();
-    // emit "UPDATE_USER_LIST" after user is removed from "users" object
-    io.emit('UPDATE_USER_LIST', {users});
-  });
-
-  socket.on('DRAW_POINTS', ({points, color, toUser}) => {
-    if (toUser) {
-      socket.broadcast.to(users[toUser]).emit('DRAW_POINTS', {points, color});
-    } else {
-      // if "toUser", emit even only to that user
-      socket.broadcast.emit('DRAW_POINTS', {points, color});
-    }
-  });
-
-});
+io.on('connection', onSocketConnect(io));
